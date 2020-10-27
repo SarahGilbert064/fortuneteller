621bdb05be16ebec93ce3f39777f942eabac7e2b
Ellie Lambert <ebezjian@gmail.com>
2020-10-27 16:55:08 -0400

    add html to index.html including possible selections for fortune teling
    Co-authored-by: Sarah Gilbert <sarahgilbert064@gmail.com>

---
+++ js/script.js
@@ -0,0 +1,10 @@
+for (let index=0; index <=30; index += 1) {
+  console.log(index)
+}
+
+let array = [0,4,5,7,10];
+let doubleArray = [];
+for (let index=0; index < array.length; index +=1) {
+  doubleArray.push(array[index]*2);
+}
+doubleArray;
